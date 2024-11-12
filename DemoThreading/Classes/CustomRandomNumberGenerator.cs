10cc1cbed75c3fc2bdbea9afe821a2aa35a86d52
Aeligeti Greeshma <greeshma425a@gmail.com>
2024-11-12 16:58:00 +0530

    Converted application into MVVM model.

--- DemoThreading/Classes/CustomRandomNumberGenerator.cs
+++ DemoThreading/Classes/CustomRandomNumberGenerator.cs
@@ -5,34 +5,30 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace DemoThreading.Classes
 {
 
-    public class ClassA
+    public class CustomRandomNumberGenerator
     {
         private Random _random;
 
         public event Action<int> NumberGenerated;
         private CancellationTokenSource _cancellationTokenSource;
-
-        public ClassA()
+       
+        public CustomRandomNumberGenerator()
         {
             _random = new Random();
-          
-        }
+              }
 
-        public void GenerateNumbers()
+        public void GenerateNumbers(int LowerLimit,int UpperLimit)
         {
             _cancellationTokenSource = new CancellationTokenSource();
 
 
-            Task.Run(async() =>
+            Task.Run(() =>
             {
                 while (!_cancellationTokenSource.Token.IsCancellationRequested)
                 {
-
-                    int randomNumber = _random.Next(1, 2001); // Random number between 1 and 2000
+                    int randomNumber = _random.Next(LowerLimit, UpperLimit); // Random number between Lower limit and upper limit.
                     NumberGenerated?.Invoke(randomNumber);
-                    Thread.Sleep(500); // Wait for 500ms between numbers
-
-
+                     Thread.Sleep(500);
                 }
                 
             }, _cancellationTokenSource.Token);
--- DemoThreading/MainWindow.xaml.cs
+++ DemoThreading/MainWindow.xaml.cs
@@ -1,162 +1,33 @@
-﻿using DemoThreading.Classes;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DemoThreading
 {
     public partial class MainWindow : Window
     {
-        private ClassA classA;
-        private ClassB classB;
-        public ObservableCollection<int> AllNumbersList { get; set; }
-        public ObservableCollection<int> NumbersList { get; set; } // Observable collection for ListView
-        private bool _isUpdatingText = false;
+        private MainWindowViewModel viewModel;
+
         public MainWindow()
         {
 
             InitializeComponent();
-            btnStop.IsEnabled = false;
-
-            AllNumbersList = new ObservableCollection<int>();
-
-            NumbersList = new ObservableCollection<int>();
-
-            // Set the DataContext to allow binding in XAML
-            DataContext = this;
-
-            // Initialize ClassA and ClassB
-            classA = new ClassA();
-            classB = new ClassB();
-
-            AllNumbersList.CollectionChanged += AllNumbersList_CollectionChanged;
-            NumbersList.CollectionChanged += MultipleList_CollectionChanged;
-
-
-
-            classA.NumberGenerated += OnNumberGenerated;
-            classB.DivisibleNumberFound += OnDivisibleNumberFound;
-        }
-        private void OnNumberGenerated(int number)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                AllNumbersList.Add(number);
-                int inputNumber;
-                if (int.TryParse(inputTextBox.Text, out inputNumber) && inputNumber != 0)
-                {
-                    classB.CheckNumber(number, inputNumber);
-                }
-            });
-        }
-
-        private void OnDivisibleNumberFound(int number)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                NumbersList.Add(number);
-            });
-        }
-        private void AllNumbersList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                // Use the Dispatcher to ensure ScrollIntoView is called after the UI updates
-                Dispatcher.BeginInvoke((Action)(() =>
-                {
-                    allNumsListView.ScrollIntoView(AllNumbersList[AllNumbersList.Count - 1]);
-                }), System.Windows.Threading.DispatcherPriority.Background);
-            }
-        
-    }
-
-        private void MultipleList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                // Use the Dispatcher to ensure ScrollIntoView is called after the UI updates
-                Dispatcher.BeginInvoke((Action)(() =>
-                {
-                    multipleNumsListView.ScrollIntoView(NumbersList[NumbersList.Count - 1]);
-                }), System.Windows.Threading.DispatcherPriority.Background);
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+            
+            viewModel.ScrollAllNumbersToEnd +=()=> ScrollListViewToEnd(allNumsListView);
+            viewModel.ScrollFilteredNumbersToEnd +=()=> ScrollListViewToEnd(multipleNumsListView);
+            
             }
 
-        }
-
-        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        private void ScrollListViewToEnd(ListView listView)
         {
-            if (string.IsNullOrWhiteSpace(inputTextBox.Text))
+            if (listView.Items.Count > 0)
             {
-                MessageBox.Show("Enter Number to see the results", "Empty Input", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
+                listView.ScrollIntoView(listView.Items[listView.Items.Count - 1]);
             }
-
-            AllNumbersList.Clear();
-            NumbersList.Clear();
-            btnStart.IsEnabled = false;
-            btnStop.IsEnabled = true;
-            classA.GenerateNumbers();
-        
         }
 
-        private void BtnStop_Click(object sender, RoutedEventArgs e)
-        {
-
-            btnStart.IsEnabled = true;
-            btnStop.IsEnabled = false;
-            classA.StopGeneratingNumbers();
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            classA.StopGeneratingNumbers();
-        }
-
-        private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-            int inputNumber;
-
-            if (_isUpdatingText)
-                return;
-
-            //NumbersList.Clear();
-            if (!int.TryParse(inputTextBox.Text, out _))
-            {
-                _isUpdatingText = true;
-                // If not an integer, show a warning message
-                MessageBox.Show("Please enter a valid integer.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
-
-                // Clear the TextBox to remove invalid input
-                inputTextBox.Text = string.Empty;
-
-                // Move the cursor to the beginning of the TextBox
-                inputTextBox.CaretIndex = 0;
-                _isUpdatingText = false;
-
-                return;
-            }
-
-            if (int.TryParse(inputTextBox.Text, out inputNumber) && inputNumber != 0)
-            {
-                foreach (var number in AllNumbersList)
-                {
-                    classB.CheckNumber(number, inputNumber); // Pass the divisor to ClassB for filtering
-                }
-            }
-
-        }
+       
+       
     }
 }
